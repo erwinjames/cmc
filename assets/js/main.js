5c87d5588dbc89cf525affb277e12eed012cc551
erwinjames <manugasewinjames@gmail.com>
2020-08-07 10:07:46 +0800

    carrer embeded

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,36 @@
         });
 
 })(jQuery);
+
+
+
+
+
+$("#slideshows > img:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshows > img:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(3000)
+    .end()
+    .appendTo('#slideshows');
+},  5000);
+
+$( document ).ready(function() {
+   
+	var timerId=setInterval(function() { 
+	
+		$('h3.whr-title').each(function() {
+			
+		  var href=$(this).children().first().attr('href');
+		  		  
+		  var new_content=$(this).parent().html() + "<div class='apply-now-btn'><a href='"+ href +"' target='_blank'>Apply Now</a></div>";
+		  
+		  $(this).parent().html(new_content) ;
+		  
+		  clearInterval(timerId);
+		});
+		
+	},  1000);
+});
