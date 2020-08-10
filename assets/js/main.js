db4805c9afc20c55c1fe3d4fb2912befee58e4bb
erwinjames <manugasewinjames@gmail.com>
2020-08-10 11:40:21 +0800

    update in loans_product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,3 +90,22 @@ $( document ).ready(function() {
 		
 	},  1000);
 });
+
+$(document).ready(function() {
+    $("#toggle").click(function() {
+      var elem = $("#toggle").text();
+      if (elem == "Read More") {
+        $("#toggle").text("Read Less");
+        $("#text").slideDown();
+      } else {
+      
+        $("#toggle").text("Read More");
+        $("#text").slideUp();
+      }
+    });
+  });
+
+  $('.modal-toggle').on('click', function(e) {
+    e.preventDefault();
+    $('.modal').toggleClass('is-visible');
+  });
