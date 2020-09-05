c3e0c52bfc10d34d23ea5f042c27479273c5f07e
erwinjames <manugasewinjames@gmail.com>
2020-09-05 13:25:22 +0800

    organaizational update

--- coop_organizational.php
+++ coop_organizational.php
@@ -96,176 +96,31 @@
         <!-- Main -->
         <section class="wrapper style1">
             <div class="container">
-                <div class="row gtr-200">
-                    <div class="col-4 col-12-narrower">
-                        <div id="sidebar">
-
-                            <!-- Sidebar -->
-
-                            <section>
-                                <h3>What is Cooperative?</h3>
-                                <p>A cooperative is a duly registered association of PERSONS, with a common bond of interest who have voluntarily joined together to achieve a lawful common social or economic end, and making equitable contributions to the
-                                    capital required and accepting a fair share of the risks and benefits of the undertaking in accordance with universally accepted cooperative principles.
-
-                                </p>
-                                <footer>
-                                    <!--   <a href="#" class="button">Continue Reading</a> -->
-                                </footer>
-                            </section>
-
-                            <section>
-                                <h3>Cooperative Principles</h3>
-                                <span class="image featured"><img src="images/principles.png" alt="" /></span>
-
-                                <!--     <ul class="links">
-                                    <li><a href="#">Amet turpis, feugiat et sit amet</a></li>
-                                    <li><a href="#">Ornare in hendrerit in lectus</a></li>
-                                    <li><a href="#">Semper mod quis eget mi dolore</a></li>
-                                    <li><a href="#">Consequat etiam lorem phasellus</a></li>
-                                    <li><a href="#">Amet turpis, feugiat et sit amet</a></li>
-                                    <li><a href="#">Semper mod quisturpis nisi</a></li>
-                                </ul> -->
-                                <footer>
-                                    <h3>Cooperative Development Authority (CDA</h3>
-                                    <p>
-                                        RA 6938 in acted in March 10, 1990 Authorized the Organization of Cooperatives of the Philippines
-
-                                    </p>
-                                    <span class="image featured"><img src="images/cda.jpg" alt="" /></span>
-                                    <!--   <a href="#" class="button">More Random Links</a> -->
-                                </footer>
-                            </section>
-
-                        </div>
-                    </div>
-                    <div class="col-8  col-12-narrower imp-narrower">
-                        <div id="content">
-
-                            <!-- Content -->
-
-                            <article>
-                                <header>
-                                    <h2>Cordova Multipurpose Cooperative History</h2>
-                                    <p>How CMC Started?</p>
-                                </header>
-
-                                <span class="image featured"><img src="images/history.png" alt="" /></span>
-
-                                <h3>October 30-31 & November 1, 1971</h3>
-                                <p> Rev. Fr. Rudy Abao, MSC initiated Self-discovery Seminar to 25 Parishioners</p>
-                                <p> November 15 to December 11, 1971- 1st PMES to 25 participants was conducted </p>
-
-                                <h3>December 12, 1971</h3>
-                                <p>1st Organizational Meeting elected officers of the cooperative or the ad-hoc officers, initial capital was P250.00
-                                </p>
-                                <h3>March 14, 1972</h3>
-
-                                <p>Registered as Pre-coop at the Cooperative Administration Office with the name CORDOVA COOPERATIVE CREDIT UNION
-                                </p>
-                                <h3>March 14, 1972</h3>
-                                <p>Registered as full-fledged Coop with the name CORDOVA CREDIT COOPERATIVE INC(CCCI)
-                                </p>
-
-                                <h3>July 5, 1978</h3>
-                                <p>Consumer Store was opened, due to members demand the capital was from member’s dividend & patronage refund
-                                </p>
-                                <h3>July 9, 1981</h3>
-                                <p>Consumer store was registered as a separate coop and was named as CORDOVA CONSUMERS COOPERATIVE INC. (CCONCI)
-                                </p>
-
-                                <h3>June 19,1993</h3>
-                                <p>Consolidation of CCCI & CCONCI with the new name CORDOVA MULTIPURPOSE COOPERATIVE
-                                </p>
-                                <h3>
-                                    <p>December 13, 1993- Opening of Lapu-lapu Branch</p>
-                                </h3>
-                                <h3>
-                                    <p>March 28, 2007 - Opening of Basak Branch</p>
-                                </h3>
-                                <h3>
-                                    <p>October 30, 2008 - Opening of Olango Branch</p>
-                                </h3>
-                                <h3>
-                                    <p>September 8, 2016 - Opening of Mandaue Satellite Office</p>
-                                </h3>
-                            </article>
-
-                        </div>
-                    </div>
+                <div class="row">
+                <span class="image featured"><img src="images/org.png" alt="" /></span>
                 </div>
             </div>
         </section>
 
         <!-- Footer -->
-        <div id="footer">
-            <div class="container">
-                <div class="row">
-                    <section class="col-3 col-6-narrower col-12-mobilep">
-                        <h3>BRANCHES</h3>
-                        <ul class="links">
-                            <li><a href="#">MAIN BRANCH</a></li>
-                            <li><a href="#">LAPU-LAPU BRANCH</a></li>
-                            <li><a href="#">BASAK BRANCH</a></li>
-                            <li><a href="#">OLANGO BRANCH</a></li>
-                            <li><a href="#">MANDAUE OFFICE</a></li>
-
-                        </ul>
-                    </section>
-                    <section class="col-6 col-12-narrower">
-                        <h3>Get In Touch</h3>
-                        <form>
-                            <div class="row gtr-50">
-                                <div class="col-6 col-12-mobilep">
-                                    <input type="text" name="name" id="name" placeholder="Name" />
-                                </div>
-                                <div class="col-6 col-12-mobilep">
-                                    <input type="email" name="email" id="email" placeholder="Email" />
-                                </div>
-                                <div class="col-12">
-                                    <textarea name="message" id="message" placeholder="Message" rows="5"></textarea>
-                                </div>
-                                <div class="col-12">
-                                    <ul class="actions">
-                                        <li><input type="submit" class="button alt" value="Send Message" /></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </form>
-                    </section>
-                </div>
-            </div>
-
-            <!-- Icons -->
-            <ul class="icons">
-                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-                <li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-                <li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-                <li><a href="#" class="icon brands fa-google-plus-g"><span class="label">Google+</span></a></li>
-            </ul>
-
-            <!-- Copyright -->
-            <div class="copyright">
-                <ul class="menu">
-                    <li>&copy; CORDOVAMPC. All rights reserved</li>
-                </ul>
-            </div>
-
-        </div>
+        <?php include "includes/footer.php"?>
     </div>
 
     <!-- Scripts -->
    <!-- <script src="assets/js/jquery.min.js"></script> -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-   <script src="assets/js/jquery.dropotron.min.js"></script>
-   <script src="assets/js/browser.min.js"></script>
-   <script src="assets/js/breakpoints.min.js"></script>
-   <script src="assets/js/util.js"></script>
-   <script src="assets/js/main.js"></script>
-   <script src="assets/js/carousel.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
-   <script src="assets/js/loading.js"></script>
+   <script src="assets/js/jquery.min1.js"></script>
+  <script src="assets/js/jquery.dropotron.min.js"></script>
+  <script src="assets/js/browser.min.js"></script>
+  <script src="assets/js/breakpoints.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+  <script src="assets/js/carousel.js"></script>
+  <script src="assets/js/modernizr.js"></script>
+  <script src="assets/js/loading.js"></script>
 
 </body>
-
+<script src="assets/js/aos.js"></script>
+<script>
+    AOS.init();
+  </script>
 </html>
