3a41833cc1b00fbb94053834e2ae700d76c212fc
erwinjames <37238531+erwinjames@users.noreply.github.com>
2020-09-14 16:53:11 +0800

    initial commit

---
+++ sub_admin/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sub Admin</title>
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    <header id="header">
+            <div class="logo">
+                    <a href=""><img src="" alt=""><span>CORDOVA MULTIPURPOSE COOPERATIVE</span></a>
+            </div>
+
+            <div class="profile">
+               <a href=""><i class="fa fa-user" aria-hidden="true"></i></a>
+            </div>
+    </header>
+                <a class="mobile"><i class="fa fa-bars" aria-hidden="true"></i></a>
+                <div id="container">
+                    <div class="sidebar">
+                        <ul id="nav">
+                            <li>
+                            <i class="fa fa-tachometer" aria-hidden="true"></i>
+                            <a href="" class="selected">Dashboard</a>
+                            </li>
+                            <li>
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                            <a href="">Post News</a>
+                            </li>
+                            <li>
+                            <li>
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                            <a href="">career</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="content">
+                        <h1>Dashboard</h1>
+                    
+
+                    </div>
+    </div>
+        
+</body>
+<!-- script =============================================-->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+<!-- main js -->
+ <script src="assets/js/main.js"></script>
+</html>
