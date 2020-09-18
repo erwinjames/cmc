b667b06ca2ce2b15346349058d9417407ce6316f
erwinjames <manugasewinjames@gmail.com>
2020-09-18 20:11:49 +0800

    initial commit

--- sub_admin/index.php
+++ sub_admin/index.php
@@ -7,6 +7,8 @@
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-red.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.6.0/tailwind.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css">
 <link rel="stylesheet" href="assets/css/main.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
@@ -71,13 +73,60 @@
 
     </div> 
         <div class="content">
-        
-        
+        <div class="bg-gray-200 flex items-center justify-center px-5 py-5">
+    <div class="w-full max-w-3xl">
+        <div class="-mx-2 md:flex">
+            <div class="w-full md:w-1/3 px-2">
+                <div class="rounded-lg shadow-sm mb-4">
+                    <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                        <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                            <h4 class="text-sm uppercase text-gray-500 leading-tight">Users</h4>
+                            <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">3,682</h3>
+                            <p class="text-xs text-green-500 leading-tight">▲ 57.1%</p>
+                        </div>
+                        <div class="absolute bottom-0 inset-x-0">
+                            <canvas id="chart1" height="70"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 px-2">
+                <div class="rounded-lg shadow-sm mb-4">
+                    <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                        <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                            <h4 class="text-sm uppercase text-gray-500 leading-tight">Subscribers</h4>
+                            <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">11,427</h3>
+                            <p class="text-xs text-red-500 leading-tight">▼ 42.8%</p>
+                        </div>
+                        <div class="absolute bottom-0 inset-x-0">
+                            <canvas id="chart2" height="70"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 px-2">
+                <div class="rounded-lg shadow-sm mb-4">
+                    <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+                        <div class="px-3 pt-8 pb-10 text-center relative z-10">
+                            <h4 class="text-sm uppercase text-gray-500 leading-tight">Comments</h4>
+                            <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">8,028</h3>
+                            <p class="text-xs text-green-500 leading-tight">▲ 8.2%</p>
+                        </div>
+                        <div class="absolute bottom-0 inset-x-0">
+                            <canvas id="chart3" height="70"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+</div>      
+</div>
 </section>
 </body>
 <!-- script =============================================-->
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 <!-- main js -->
- <!-- <script src="assets/js/main.js"></script> -->
+ <script src="assets/js/main.js"></script>
 </html>
