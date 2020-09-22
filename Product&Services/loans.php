7107a926c42e09652f9309c31004b0c5c38bea1d
erwinjames <manugasewinjames@gmail.com>
2020-09-22 15:56:05 +0800

    changes sa page sa products

---
+++ Product&Services/loans.php
@@ -0,0 +1,186 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Cordova Multipupose Cooperative</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="../assets/css/main.css" />
+</head>
+
+<body class="is-preload">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <?php include "../includes/header2.php";?>
+            <!-- Nav -->
+          <?php include "savings-menu.php"?>
+        </div>
+
+        <!-- Main -->
+        <section class="wrapper style1">
+            <div class="container">
+                <div class="row gtr-200">
+                    <div class="col-4 col-12-narrower">
+                        <div id="sidebar">
+                        <section> 
+                                <h2>Loans</h2>
+                            <nav id="nav" >
+                                   <ul >
+                                        <li style="margin-left:14px;" >
+                                            <a href="Regular.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Savings Deposit</a>
+                                         </li>
+                                         <li>
+                                            <a href="TimeDeposit.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Time Deposit</a>
+                                        </li>
+                                        <br>
+                                        <li class="current active">
+                                            <a href="loans.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Loans</a>
+                                        </li>
+                                        <br>
+                                        <li>
+                                            <a href="Yulabco.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Members Benifits</a>
+                                        </li>
+                                        <br>
+                                        <li>
+                                            <a href="Yulabco.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Bussines Enterprise</a>
+                                        </li>
+                                     </ul>
+                            </nav>
+                            <br>
+                                <footer>
+                                    <p>SERVES WITH A HEART</p>
+                                    <!--   <a href="#" class="button">Continue Reading</a> -->
+                                </footer>
+                    </section>
+
+                        </div>
+                    </div>
+                    <div class="col-8  col-12-narrower imp-narrower">
+                        <div id="content">
+                            <article>
+                                    <h3>Providential Loans</h3>
+                                    <button class="colapse">Appliance Loan</button>
+                                     <div class="contenting">
+                                        <p>•	Member with at least 20% share capital of the aggregate amount applied</p>
+                                        <p>•	Member must not have a past due loan amortization on other loans in the Coop</p>
+                                        <p>•	Original ownership & warranty Certificate will be surrendered to the cooperative</p>
+                                        <p>•	Flexible term: Max. of 36 months</p>
+                                        <p>•	Mode of Payment:  Bi-Monthly & Monthly</p>
+                                    </div>
+                                    <button class="colapse">Salary Loan</button>
+                                     <div class="contenting">
+                                        <p>•	Member with at least 20% share capital of the aggregate amount applied</p>
+                                        <p>•	Member must not have a past due loan amortization on other loans in the Coop</p>
+                                        <p>•	Member must be a regular employee of any government/private company</p>
+                                        <p>•	For Those with ATM: Member must surrender his/her Salary ATM Card</p>
+                                        <p>•	For Payroll Deduction: There must be an existing MOA between the coop and The employee’s employer & authority to Deduct Salary signed by borrower </p>
+                                        <p>•	Flexible Term: Max. of 36 months</p>
+                                        <p>•	Mode of Payment:  Bi-Monthly & Monthly</p>
+                                    </div>
+                                    <button class="colapse">Educational Loan</button>
+                                     <div class="contenting">
+                                        <p>•	Member with at least 20% share capital of the aggregate amount applied</p>
+                                        <p>•	Member must not have a past due loan amortization on other loans in the Coop</p>
+                                        <p>•	Member must have an identifiable education needs</p>
+                                        <p>•	Assessment from school, Statement of Account from School, Quotation from sastre/suppliers for uniforms, canvass from bookstores/department stores</p>
+                                        <p>•	Flexible Term: Max. of 36 months</p>      
+                                        <p>•	Mode of Payment:  Bi-Monthly & Monthly</p>  
+                                    </div>
+                                    <button class="colapse">Pensioners Loan</button>
+                                     <div class="contenting">
+                                        <p>•	Member with at least 20% share capital of the aggregate amount applied</p>           
+                                        <p>•	Member must not have a past due loan amortization on other loans in the Coop</p>
+                                        <p>•	Member must be a bonafide pensioner either by SSS/GSIS</p>
+                                        <p>•	Must present his/her Pension Voucher & Letter from SSS/GSIS granting pension benefit</p>
+                                        <p>•	Member must surrender his/her Pension ATM Card/Passbook in the Coop</p>
+                                        <p>•	Health of the pensioner must be taken into consideration upon loan approval</p>
+                                        <p>•	Loan must be fully/partial secured either by Real, Chattel / Personal properties</p>
+                                        <p>•	Flexible Term: Max. of 36 months</p>
+                                        <p>•	Mode of Payment: Monthly</p>
+                                    </div>
+                                    <button class="colapse">Vehicle Loan</button>
+                                     <div class="contenting">
+                                        <p>•	Member with at least 20% share capital of the aggregate amount applied</p>
+                                        <p>•	Member must not have a past due loan amortization on other loans in the Coop</p>
+                                        <p>•	Member must have a vehicle need</p>
+                                        <p>•	Original Registration (OR’s), Certificate or Registration (CR’s) & Franchise will be surrender to the Coop</p>
+                                        <p>•	Certificate of Registration must be Encumbered in favor to the Cooperative</p>
+                                        <p>•	Flexible Term: Max. of 36 months</p>
+                                        <p>•	Mode of Payment:  Daily, Weekly, Bi-Monthly & Monthly</p>
+                                    </div>
+                                    <button class="colapse">Multipurpose Loan</button>
+                                     <div class="contenting">
+                                        <p>•	Member with at least 20% share capital of the aggregate amount applied</p>
+                                        <p>•	Member must not have a past due loan amortization on other loans in the Coop</p>
+                                        <p>•	Member must have a need not in conformity with the existing loan products. Needs could be any one, two or more of the following:</p>
+                                        <p>&nbsp &nbsp &nbsp -Travel Expenses</p>
+                                        <p>&nbsp &nbsp &nbsp - Hospitalization</p>
+                                        <p>&nbsp &nbsp &nbsp - Maternity Expenses </p> 
+                                        <p>&nbsp &nbsp &nbsp - Payment of the financial obligation & Other Valid Purpose</p>                      
+                                        <p>•	Flexible Term: Max. of 36 months</p>
+                                        <p>•	Mode of Payment: Daily, Weekly, Bi-Monthly & Monthly</p>
+                                    </div>
+                                    <h3>Productive Loans</h3>
+                                    <button class="colapse">Working Capital</button>
+                                     <div class="contenting">
+                                    <h4>General Eligibility Requirements:</h4>
+                                    <pre style="margin-left:-6em;">
+                                    - Must be a regular member of Cordova Multipurpose Cooperative
+                                    - Member has complied the minimum amount required for Share Capital and Savings Deposit
+                                    - Member should have an establish business enterprise that he or she manages
+                                    </pre>
+                                    <h4>Working Capital Need could be the following</h4>
+                                    <pre style="margin-left:-6em; ">
+                                    - Additional capital for business;
+                                    - Merchandise Inventories such as groceries, lumber materials, RTW’s, pork or chicken meats, 
+                                        fruits and vegetables, fish, flour, rice, and other goods for use or for sale depending on the 
+                                        type of business.
+                                    </pre>
+                                    <h4>Basic Processing Requirements Needed</h4>
+                                    <pre style="margin-left:-6em; ">
+                                    - 5 sets of complete Loan Application and other documents (Co-maker, Promissory Notes, Mortgage documents if with collateral)
+                                    - Valid Business Permit
+                                    - Statement of Income and Expenses if there’s any;
+                                    </pre>
+                                    </div>
+                                    <button class="colapse">Regular Loan Interest Rates and Charges</button>
+                                     <div class="contenting">
+                                        <h3>Interest Rate (Diminishing Method: based on diminishing principal balance)</h3>
+                                        <p>&nbsp &nbsp &nbsp - Fully Secured – 2.83% per month </p>
+                                        <p>&nbsp &nbsp &nbsp - Partially Secured – 3.00% per month</p>
+                                        <p>&nbsp &nbsp &nbsp - Unsecured – 3.16% per month</p>
+                                        <h3>Loan Charges</h3>
+                                        <p>&nbsp &nbsp &nbsp - 1% Share Capital Retention Fee</p>
+                                        <p>&nbsp &nbsp &nbsp - 1% Savings Deposit</p>
+                                        <p>&nbsp &nbsp &nbsp - 1% Consumer Goods</p>
+                                        <p>&nbsp &nbsp &nbsp - 3% Service Fee</p>
+                                        <p>&nbsp &nbsp &nbsp - 3.5% for mortgage fee of the collateral if there is</p>
+                                        <p>&nbsp &nbsp &nbsp - Loan Protection Insurance</p>
+                                    </div>
+                                    
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <?php include "../includes/footer.php"?>
+
+
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/jquery.dropotron.min.js"></script>
+    <script src="../assets/js/browser.min.js"></script>
+    <script src="../assets/js/breakpoints.min.js"></script>
+    <script src="../assets/js/util.js"></script>
+    <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
--- assets/js/main.js
+++ assets/js/main.js
@@ -129,4 +129,17 @@ $(document).ready(function() {
   });
   
 
-  
+  var coll = document.getElementsByClassName("colapse");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
