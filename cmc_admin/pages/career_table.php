7a8459e3867c860b816377329e5b7aa864a39203
erwinjames <manugasewinjames@gmail.com>
2020-09-23 16:06:28 +0800

    initial commit

--- cmc_admin/pages/career_table.php
+++ cmc_admin/pages/career_table.php
@@ -130,7 +130,7 @@
                             <tr>
                                 <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Career Title</th>
                                 <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Description</th>
-                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Date</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Date/time</th>
                                 <th class="px-6 py-3 border-b-2 border-gray-300"></th>
                             </tr>
                         </thead>
