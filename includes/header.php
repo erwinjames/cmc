e2f0d4a3ac1ed2a548ac968d9c70abc8250226fd
erwinjames <manugasewinjames@gmail.com>
2020-10-01 10:58:40 +0800

    initial commit

--- includes/header.php
+++ includes/header.php
@@ -2,12 +2,22 @@
 
 <!-- Logo -->
 <div class="row">
-<div class="col-2">
+    <div class="col-1 header_image">
         <img src="images/cmc.png" alt="">
     </div>
-    <div class="col-8 header_tag">
-      <span><h1 style="font-size:60px;font-family:'Times New Roman', Times, serif;color:rgb(16, 157, 238);text-decoration: none;">Cordova Multipurpose Cooperative</h1></span>
+    <div class="col-5 header_tag">
+      <span><h1 style="font-size:40px;font-family:'Times New Roman', Times, serif;color:rgb(16, 157, 238);text-decoration: none;">Cordova Multipurpose Cooperative</h1></span>
       <span><small>Cda Registration No. 9520-07007277</small></span>
+    </div>
+    <div class="social__header col-6">
+    <ul class="social__icons">
+                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands fa-google-plus-g"><span class="label">Google+</span></a></li>
+      </ul>
+      <div class="header__search">
+        <!-- ------------------------------------search -->
+      </div>
     </div>
 </div>
 
