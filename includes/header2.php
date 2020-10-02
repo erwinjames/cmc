12811dbb73dc7c9cf0d6e541b6aa076433bf2c39
erwinjames <manugasewinjames@gmail.com>
2020-10-02 19:03:26 +0800

    initial commit

--- includes/header2.php
+++ includes/header2.php
@@ -1,22 +1,22 @@
 <div id="header">
 
 <!-- Logo -->
-<div class="row">
+<div class="row top__header">
     <div class="col-1 header_image">
         <img src="../images/cmc.png" alt="">
     </div>
-    <div class="col-5 header_tag">
-      <span><h1 style="font-size:40px;font-family:'Times New Roman', Times, serif;color:rgb(16, 157, 238);text-decoration: none;">Cordova Multipurpose Cooperative</h1></span>
+    <div class="col-7 header_tag">
+      <span><h1 style="font-family:'Times New Roman', Times, serif;color:rgb(16, 157, 238);text-decoration: none;">Cordova Multipurpose Cooperative</h1></span>
       <span><small>Cda Registration No. 9520-07007277</small></span>
     </div>
-    <div class="social__header col-6">
+    <div class="social__header col-2">
     <ul class="social__icons">
                
                 <li><a href="https://www.facebook.com/cordovampc/?eid=ARB2CoSolJopsB31OKkwWLqy7O1dqotM5RXdS3IzsqZaVJTvloyttt2EK0BccX5cnziAMxc1U1r1TR00" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
                 <li><a href="mailto:Cordovacoop2020@gmail.com" class="icon brands fa-google-plus-g"><span class="label">Google+</span></a></li>
       </ul>
       <div class="header__search">
-        <!-- ------------------------------------search -->
+    
       </div>
     </div>
 </div>
