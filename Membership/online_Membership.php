92ff8569ad99c111df2af7be8ef51bc407ccdb13
erwinjames <manugasewinjames@gmail.com>
2020-10-05 15:55:10 +0800

    initial commit

--- Membership/online_Membership.php
+++ Membership/online_Membership.php
@@ -71,8 +71,8 @@
                             <div class="col-12 col-12-narrower download__file">
                             <h2>STEP 5</h2>
                             <h6>Scan the deposit slips and send through email as proof of deposit.</h6>
-                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="../downloadable/signature_card.pdf">Download Membership Form</a> <br>
-                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="#">Signature Card</a>
+                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="#">Download Membership Form</a> <br>
+                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="../downloadable/signature_card.pdf">Signature Card</a>
                             </div>
                             
                     </div>
