38c34f0155777f53c4e8300a0a8a5e166570a233
erwinjames <manugasewinjames@gmail.com>
2020-10-05 10:41:28 +0800

    initial commit

--- Membership/online_Membership.php
+++ Membership/online_Membership.php
@@ -36,21 +36,43 @@
                             <h5>Online Membership</h5>
                             <div class="col-12 col-12-narrower">
                                 <h2>STEP 1</h2>
-                                 <span> Attend one(1) hour Pre-Membership Orientation from Monday to Saturday at <b>10:00 to 11:00 AM</b and 1:00 to 2:00 PM.</span>                      
+                                <h4>Document Needed</h4>
+                                <span class="bullets"><i class="fa fa-check"></i> &nbsp; Membership Application</span> <br> 
+                                  <span class="bullets"><i class="fa fa-check"></i> &nbsp;  Signature Card</span> <br> 
+                                  <span class="bullets"><i class="fa fa-check"></i> &nbsp; 2×2 ID Picture</span> <br> 
+                                  <span class="bullets"><i class="fa fa-check"></i> &nbsp;  Photocopy of valid ID (TIN,SSS, and other government issued ID)</span> <br> 
+                                                    
                             </div>
-                           
+                           <br>
                             <div class="col-12 col-12-narrower">
                                 <h2>STEP 2</h2>
-                                <h6>Payments and Deposit:</h6>
-                                  <span class="bullets"><i class="fa fa-check"></i> &nbsp; Membership Fee P <b>100.00</b></span> <br> 
+                                <h6>Send the scanned copy of your documents</h6>
+                                  <span class="bullets"><i class="fa fa-envelope"></i> &nbsp;&nbsp; Cordovacoop2020@gmail.com</span> <br> 
+                                         
+                            </div>
+                            <br>
+                            <div class="col-12 col-12-narrower">
+                                <h2>STEP 3</h2>
+                                <h6>Wait for Cordova Cooperative Confirmation & Instruction through official email</h6>
+                                  <span class="bullets"><i class="fa fa-envelope"></i> &nbsp;&nbsp; Cordovacoop2020@gmail.com</span> <br>    
+                            </div>
+                            <br>
+                            <div class="col-12 col-12-narrower">
+                                <h2>STEP 4</h2>
+                                <h6>Payments and Deposits through Bank or Remittance Company</h6>
+                                <span class="bullets"><i class="fa fa-check"></i> &nbsp; Membership Fee P <b>100.00</b></span> <br> 
                                   <span class="bullets"><i class="fa fa-check"></i> &nbsp;  Initial Share Capital Deposit P <b>1250</b></span> <br> 
                                   <span class="bullets"><i class="fa fa-check"></i> &nbsp; Initial Savings P<b>500.00</b></span> <br> 
                                   <span class="bullets"><i class="fa fa-check"></i> &nbsp;  Funos P <b>720.00</b></span> <br> 
-                                  <span class="bullets"><i class="fa fa-check"></i> &nbsp; Health Care (optional) P <b>500</b></span> <br>                     
+                                  <span class="bullets"><i class="fa fa-check"></i> &nbsp; Health Care (optional) P <b>500</b></span> <br>      
                             </div>
-
-                            <div class="download__file">
-                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="#">Download Membership Form</a>
+                            <br>
+                            
+                            <div class="col-12 col-12-narrower download__file">
+                            <h2>STEP 5</h2>
+                            <h6>Scan the deposit slips and send through email as proof of deposit.</h6>
+                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="#">Download Membership Form</a> <br>
+                            <i class="fas fa-file-download"></i>&nbsp;&nbsp;<a href="#">Signature Card</a>
                             </div>
                             </div>
                     </div>
