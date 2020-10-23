141b03cfe1af13a55263e79e0e88c9e3aa8b49b8
erwinjames <manugasewinjames@gmail.com>
2020-10-23 15:54:06 +0800

    pre membership orientation

--- Membership/online-pre-membership-orientation.php
+++ Membership/online-pre-membership-orientation.php
@@ -36,15 +36,19 @@
                             <form class="online__pre_memberhsip_container">
                             <div class="row gtr-50">
                                 <div class="col-4 col-12-mobilep">
+                                    <h6 class="online_pre_fonts">First Name *</h6>
                                     <input type="text" name="first_name" id="name" placeholder="First Name" />
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Middle Name</h6>
                                     <input type="text" name="mid_name" id="mid_name" placeholder="Middle Name" />
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Last Name</h6>
                                     <input type="text" name="Last_name" id="l_name" placeholder="Last Name" />
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Gender</h6>
                                 <div class="input-radio">
                                 <input type="radio" id="radio-1" name="sex" value="male" checked="checked">
                                 <label for="radio-1">Male</label>
@@ -55,21 +59,29 @@
                             </div>
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Birthdate *</h6>
                                 <input class="calendar" id="datepicker">
                                 </div>
                                 <div class="col-4 col-12-mobilep">
-                                <input type="email" name="Last_name" id="email" placeholder="Email" />
+                                <h6 class="online_pre_fonts">Email Address</h6>
+                                <input type="email" name="email" id="email" placeholder="Email" />
                                 </div>
                                 <div class="col-12 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Present Address *</h6>
                                 <input type="text" name="present_add" id="present_address" placeholder="Present Address" />
                                 </div>
+                              
                                 <div class="col-6 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Occupation</h6>
                                 <input type="text" name="occupation" id="occupation" placeholder="Occupation" />
                                 </div>
+                                
                                 <div class="col-6 col-12-mobilep">
+                                <h6 class="online_pre_fonts">Name of Employer/Business Name</h6>
                                 <input type="text" name="employer_name" id="employer_name" placeholder="Name of Employer/Business Name" />
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                    <h6 class="online_pre_fonts">Preferred Branch Office</h6>
                                 <select name="branch">
                                 <option value="Main">Main Branch</option>
                                 <option value="LLC">Lapu-Lapu Branch</option>
@@ -79,12 +91,14 @@
                                 </select>
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                    <h6 class="online_pre_fonts">Preferred Time (Philippine Time, PHT, GMT +8)</h6>
                                 <select name="preferd_time">
                                 <option value="10am">10:00 AM</option>
                                 <option value="1pm">1:00 PM</option>
                                 </select>
                                 </div>
                                 <div class="col-4 col-12-mobilep">
+                                    <h6 class="online_pre_fonts">Contact number *</h6>
                                 <input type="text" name="Contact_num" id="contact_num" placeholder="Contact Number" />
                                 </div>
                                 <div class="col-12 col-12-mobilep">
