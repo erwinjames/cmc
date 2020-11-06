95a7c5ecf19a68b111e99a7bdbae04d38e76eb0b
erwinjames <manugasewinjames@gmail.com>
2020-11-06 09:42:15 +0800

    initial commit

--- Product&Services/BusinessEnterprise.php
+++ Product&Services/BusinessEnterprise.php
@@ -53,11 +53,50 @@
                             <article>
                             <span class="image featured"><img src="../images/business_enterprise.png" alt="" /></span>
                             <div id="contenair_slider">
-        <div id="product_slides">
-            <div class="content_slide"><img src="" width="773" height="200"></div>
-            <div class="content_slide"><img src="" width="773" height="200"></div>
-            <div class="content_slide"><img src="" width="773" height="200"></div>
-            <div class="content_slide"><img src="" width="773" height="200"></div>
+                            <div id="product_slides">
+            <div class="content_slide"><img src="../images/businessEnterprise/p1.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p2.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p3.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p4.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p5.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p6.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p7.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p8.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p9.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p10.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p11.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p12.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p13.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p14.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p15.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p16.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p17.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p18.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p19.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p20.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p21.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p22.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p23.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p24.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p25.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p26.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p27.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p28.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p29.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p30.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p31.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p32.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p33.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p34.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p35.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p36.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p37.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p38.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p39.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p40.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p41.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p42.jpg" width="300" height="200"></div>
+            <div class="content_slide"><img src="../images/businessEnterprise/p43.jpg" width="300" height="200"></div>
         </div>
         <nav id="slider_menu">
             <ul>
@@ -85,9 +124,16 @@
                         <img src="">
                     </a>
                 </li>
+                <li class="product">
+                    <a href="">
+                        <img src="">
+                    </a>
+                </li>
+                
             </ul>
         </nav>
     </div>   
+    
    </article>
   </div>
    </div>
