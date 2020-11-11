8888d345d86688b9c8a80629804c6a10bbb0cf21
erwinjames <manugasewinjames@gmail.com>
2020-11-11 14:12:36 +0800

    initail commit

--- index.php
+++ index.php
@@ -25,7 +25,8 @@
             <li><a href="AboutUs/coop_history.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Coop Profile</a></li>
             <li><a href="AboutUs/coop_VMG.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Vision,Mission,Goals</a></li>
             <li><a href="AboutUs/coop_organizational.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Organizational Structure</a></li>
-            <li><a href="#"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Board And Officers</a>
+            <li><a href="#">
+            <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Board And Officers</a>
                <ul>
                      <li><a href="AboutUs/coop_officers.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>BOD</a></li>
                      <li><a href="AboutUs/coop_officers2.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>COMMITTEES</a></li>
@@ -66,7 +67,7 @@
             </ul>
         </li>
         <li>
-            <a href="">Membership</a>
+            <a href="#">Membership</a>
             <ul>
                 <li>
                     <a href="membership/regular_membership.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Regular Membership</a>
