fb7193f849cf0caa458eb6a468506ac5f52bb1b7
erwinjames <manugasewinjames@gmail.com>
2020-11-13 09:18:58 +0800

    initail commit

--- index.php
+++ index.php
@@ -20,17 +20,17 @@
         <ul>
                 <li class="current"><a href="index.php">Home</a></li>
                     <li>
-                    <a>About Us</a>
-                    <ul>
-                    <li><a href="AboutUs/coop_history.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Coop Profile</a></li>
-                    <li><a href="AboutUs/coop_VMG.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Vision,Mission,Goals</a></li>
-                    <li><a href="AboutUs/coop_organizational.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Organizational Structure</a></li>
-                    <li><a href="#">
-                    <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Board And Officers</a>
-                        <ul>
-                            <li><a href="AboutUs/coop_officers.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>BOD</a></li>
-                            <li><a href="AboutUs/coop_officers2.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>COMMITTEES</a></li>
-                        </ul>
+                            <a>About Us</a>
+                                <ul>
+                                        <li><a href="AboutUs/coop_history.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Coop Profile</a></li>
+                                        <li><a href="AboutUs/coop_VMG.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Vision,Mission,Goals</a></li>
+                                        <li><a href="AboutUs/coop_organizational.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Organizational Structure</a></li>
+                                        <li><a href="#">
+                                        <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>Board And Officers</a>
+                                <ul>
+                                    <li><a href="AboutUs/coop_officers.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>BOD</a></li>
+                                    <li><a href="AboutUs/coop_officers2.php"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>COMMITTEES</a></li>
+                                </ul>
                 </li>
                         <li>
                         <a href="#"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i>The Management</a>
